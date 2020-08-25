771411f1edbc3fff9e754709096a73ff73bd4819
John Graves <johntaylorgraves@gmail.com>
2020-08-25 14:43:27 +0100

    added debug script

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const readline = require('readline');
 const {google} = require('googleapis');
 
 const app = require('./app.js');
+const { SSL_OP_EPHEMERAL_RSA } = require('constants');
+
+const CALENDAR_ID = 'cepheidgaming@gmail.com'
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly https://www.googleapis.com/auth/calendar'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = 'token.json';
 
 module.exports.run = async function () {
-
-  // If modifying these scopes, delete token.json.
-  const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly https://www.googleapis.com/auth/calendar'];
-  // The file token.json stores the user's access and refresh tokens, and is
-  // created automatically when the authorization flow completes for the first
-  // time.
-  const TOKEN_PATH = 'token.json';
 
   // Load client secrets from a local file.
   fs.readFile('credentials.json', (err, content) => {
@@ -70,16 +73,81 @@ module.exports.run = async function () {
     });
   }
 
-  /**
-   * Prints the names and majors of students in a sample spreadsheet:
-   * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-   * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
-   */
   function runApp(auth) {
-    const sheets = google.sheets({version: 'v4', auth});
-    const calendar = google.calendar({version: 'v3', auth})
+    let sheets = google.sheets({version: 'v4', auth});
+    let calendar = google.calendar({version: 'v3', auth})
 
     app.run(sheets, calendar)
+
+  }
+}
+
+module.exports.nuke = async function () {
+  // Load client secrets from a local file.
+  fs.readFile('credentials.json', (err, content) => {
+    if (err) return console.log('Error loading client secret file:', err);
+    // Authorize a client with credentials, then call the Google Sheets API.
+    authorize(JSON.parse(content), resetCalendar);
+  });
+
+  /**
+   * Create an OAuth2 client with the given credentials, and then execute the
+   * given callback function.
+   * @param {Object} credentials The authorization client credentials.
+   * @param {function} callback The callback to call with the authorized client.
+   */
+  function authorize(credentials, callback) {
+    const {client_secret, client_id, redirect_uris} = credentials.installed;
+    const oAuth2Client = new google.auth.OAuth2(
+        client_id, client_secret, redirect_uris[0]);
+
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, (err, token) => {
+      if (err) return "Existing token not found or cannot be read, run once before using nuke"
+      oAuth2Client.setCredentials(JSON.parse(token));
+      callback(oAuth2Client);
+    });
+  }
+
+  async function resetCalendar(auth) {
+
+    let calendar = google.calendar({version: 'v3', auth})
+
+    console.log("Nuking the calendar")
+
+    let results = await calendar.events.list({
+      calendarId: CALENDAR_ID
+    })
+
+    let allEvents = results.data.items
+
+    for (element in allEvents) {
+
+      await sleep(250)
+
+      let thisEvent = allEvents[element]
+
+     calendar.events.delete({
+        calendarId: CALENDAR_ID,
+        eventId: thisEvent.id
+      },async function(err, event) {
+        if (err) {
+            console.log('There was an error contacting the Calendar service: ' + err);
+            return;
+        }
+        
+        console.log(`${thisEvent.id} deleted`);
+        
+         
+    });
+    }
+
+    function sleep(ms) {
+      return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+      });
+    }
+
 
   }
 }
--- update.js
+++ update.js
@@ -3,6 +3,8 @@ var cron = require('node-cron');
 const index = require('./index.js');
  
 cron.schedule('0,15,30,45 * * * *', () => {
-    console.log("Checking spreadsheet")
+
+    let time = new Date()
+    console.log("Checking spreadsheet @ " + time.getTime() + " on " + time.getDate())
     index.run();
 });
