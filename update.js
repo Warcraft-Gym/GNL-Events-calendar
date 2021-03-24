84b1ba192f78d7f20188bc3bca4ffbeea121b101
John Graves <johntaylorgraves@gmail.com>
2021-03-24 14:40:11 +0000

    fixed cronjob

--- update.js
+++ update.js
@@ -2,7 +2,7 @@ var cron = require('node-cron');
 
 const index = require('./index.js');
  
-cron.schedule('0,20,15,30,45 * * * *', () => {
+cron.schedule('0,15,30,45 * * * *', () => {
 
     let time = new Date()
     console.log("Checking spreadsheet @ " + time.getTime() + " on " + time.getDate())
