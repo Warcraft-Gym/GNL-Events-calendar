255db125683a18c079987abc049375101b722fc5
Cepheid-UK <johntaylorgraves@gmail.com>
2021-10-09 14:11:36 +0100

    creating cell ranges complete

--- src/parser.ts
+++ src/parser.ts
@@ -2,13 +2,36 @@ import { calendar_v3, sheets_v4 } from 'googleapis';
 import TimeUtils from './utils/TimeUtils';
 
 const TIMEZONE_ADJUSTMENT = new TimeUtils().getEastCoastAdjustment();
-const WEEKS = process.env.WEEKS;
-const CALENDAR_ID = process.env.CALENDAR_ID;
-const SHEET_ID = process.env.SHEET_ID_CURRENT;
+const WEEKS = process.env.WEEKS || 5;
+const CALENDAR_ID = process.env.CALENDAR_ID || '';
+const SHEET_ID = process.env.SHEET_ID_CURRENT || '';
+const CELL_RANGES = process.env.CELL_RANGES || `D6:I14 D18:I26 D30:I38`;
 
-export default function parser(
+export default async function parser(
 	sheets: sheets_v4.Sheets,
 	calendar: calendar_v3.Calendar
-): void {
-	console.log('success');
+): Promise<void> {
+	console.log('Scanning spreadsheet...');
+	console.log(CELL_RANGES?.split(' '));
+	await getAllSpreadsheetMatches(sheets, await createClanWarStrings());
+}
+
+async function getAllSpreadsheetMatches(
+	sheets: sheets_v4.Sheets,
+	clanWarStrings: string[]
+): Promise<void> {
+	console.log(clanWarStrings);
+}
+
+function createClanWarStrings(): string[] {
+	const clanWar = [] as string[];
+	const cellRanges = CELL_RANGES.split(' ');
+
+	// create 18 strings which correspond to the date and time cells for each match
+	for (let i = 1; i <= WEEKS; i++) {
+		for (let j = 0; j <= cellRanges.length - 1; j++) {
+			clanWar.push(`Week ${i}!${cellRanges[j]}`);
+		}
+	}
+	return clanWar;
 }
